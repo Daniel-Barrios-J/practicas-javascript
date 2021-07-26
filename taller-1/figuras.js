bd404182045ac3f70419d28ef09a2897169845e2
“danodevlap” <daniel.bj.rd@gmail.com>
2021-07-26 02:15:24 -0500

    organización y vinculación de paginas

--- taller-1/figuras.js
+++ taller-1/figuras.js
@@ -1,3 +1,45 @@
+// Cálculos para cuadrados
+function perimetroCuadrado(lado) {
+    return perimetro = lado*4;    
+}
+function areaCuadrado(lado) {
+    return area = lado*lado;
+}
+
+// Cálculos para cuadrados
+function perimetroTriangulo(lado1,lado2,base) {
+    return perimetro = lado1 + lado2 + base;
+} 
+
+function areaTriangulo(base,altura) {
+    return area = (base*altura)/2;
+}
+
+// Cálculos para cuadrados
+function perimetroCirculo(radio) {
+    return perimetro = radio*2*Math.PI; 
+}
+function areaCirculo(radio) {
+    return areaCirculo = radio*radio*Math.PI;
+}
+
+
+// Interacción con HTML
+
+function c_perimetroCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert("El perímetro del cuadrado es de: "+perimetro+"cm");
+}
+
+function c_areaCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert("El área del cuadrado es de: "+ area+"cm cuadrados");
+}
+// -------RETO ISOCELES--------------------
 //Esta función se llama al dar click al botón "Calcular altura"
 function calcularAltura() {
     const inputUno = document.getElementById("inputLadoUno");
--- taller-2/porcentajes.js
+++ taller-2/porcentajes.js
@@ -1,3 +1,15 @@
+function calcularDescuento() {
+    const inputPrecio = document.getElementById("precio");
+    const precio = parseFloat(inputPrecio.value);
+
+    const inputDescuento = document.getElementById("descuento");
+    const descuento = parseFloat(inputDescuento.value);
+
+    const valorFinal = (precio-(precio*descuento/100));
+    const resultado = document.getElementById("resultado");
+    resultado.innerText = "El precio final es de: $"+valorFinal;
+}
+// CUPONES---------------------
 let cupones = [
     {
         nombre: "DANO_BANANO",
@@ -9,7 +21,7 @@ let cupones = [
     }
 ]
 
-function calcularDescuento() {
+function calcularCupon() {
     const inputPrecio = document.getElementById("precio");
     const precio = parseFloat(inputPrecio.value);
 
